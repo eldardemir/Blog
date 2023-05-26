8dd535cf33b58314fefd4c522c3fa118ce749bc0
eldardemir <eldardemir31@gmail.com>
2023-05-26 18:41:09 +0200

    First Commit

---
+++ Mern/client/src/Header.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
+
+export default function Header(){
+  const[username,setUsername] = useState(null);
+  useEffect(()=>{
+    fetch('http://localhost:4000/profile',{
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo=>{
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout(){
+    fetch('http://localhost:4000/logout',{
+      credentials: 'include',
+      method: 'POST',
+    })
+  }
+return(
+<header>
+<a href="." className="logo">MyBlog</a>
+  <nav>
+    {username && (
+      <>
+      <Link to="/create">Create new post</Link>
+      <a onClick={logout}>Logout</a>
+      </>
+    )}
+    {!username && (
+      <>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      </>
+    )}
+
+  </nav>
+</header>
+);
+}
---
+++ Mern/client/src/Post.js
@@ -0,0 +1,16 @@
+export default function Post(){
+    return(
+    <div className="post">
+    <div className="image">
+    <img src="https://res.cloudinary.com/highereducation/images/w_1024,h_576,c_scale/f_auto,q_auto/v1670015316/BestColleges.com/closeup-of-python-code/closeup-of-python-code-1024x576.jpg" alt="" />
+    </div>        
+  <div className="texts">
+  <h2>Programiranje kao broj 1 zanimanje u 2023</h2>
+  <p className="info">
+  <span className="author">Eldar Demir</span>  
+  <time> 25.05.2023 13:43</time>
+  </p> 
+  </div>
+  </div>
+    )
+}
