613c9de43fad1e653b9d6aee62caff9cc0b069e1
eldardemir <eldardemir31@gmail.com>
2023-05-28 10:17:04 +0200

    Second commit

--- Mern/client/src/pages/LoginPage.js
+++ Mern/client/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {Navigate} from "react-router-dom";
+import{UserContext} from "../UserContext";
 
 export default function LoginPage () {
     const[username,setUsername]=useState('');
     const[password,setPassword]=useState('');
     const[redirect,setRedirect]=useState(false);
+    const{setUserInfo} = useContext(UserContext);
     async function login (ev){
         ev.preventDefault();
         const response = await fetch('http://localhost:4000/login',{
@@ -13,14 +15,17 @@ export default function LoginPage () {
             headers: {'Content-Type':'application/json'},
             credentials:'include',
         });
-        if(response.ok){
-            setRedirect(true);
+        if (response.ok) {
+            response.json().then(userInfo => {
+             setUserInfo(userInfo);
+              setRedirect(true);
+            });
+          } else {
+            alert('wrong credentials');
+          }
         }
-    }
     if(redirect){
         return <Navigate to={'/'} />
-    }else{
-        alert('wrong credentials');
     }
     return (
         <form className="login" onSubmit={login}>
@@ -34,5 +39,5 @@ export default function LoginPage () {
             onChange={ev=>setPassword(ev.target.value)} />
             <button>Login</button>
         </form>
-        )
+        );
 }
