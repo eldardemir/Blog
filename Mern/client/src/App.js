ff8cfd0ef80221027c984ae5536865b20aa9bc72
eldardemir <eldardemir31@gmail.com>
2023-05-29 16:20:58 +0200

    Commit number 3

--- Mern/client/src/App.js
+++ Mern/client/src/App.js
@@ -8,6 +8,8 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './UserContext';
 import CreatePost from './pages/CreatePost';
+import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/create" element={<CreatePost/>}/>
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/edit/:id" element={<EditPost />} />
         </Route>
     </Routes>
     </UserContextProvider>
